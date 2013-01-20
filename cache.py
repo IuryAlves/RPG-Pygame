aada914ca13277c8458cbdddd25b0103a3c7a867
Iury <Iury>
2013-01-20 02:08:33 -0200

    criando módulo de cache que contém todos os arquivos externos que são utilizados durante o jogo

---
+++ cache.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+'''
+Aqui ficam todos  os  arquivos que são carregados para o jogo como imagens,sons etc
+'''
+
+# listas com as imagens que dão a impressão de que o personagem se movimenta pelo cenário
+
+listaImagensFrente = ["sprites/danteFrente1.png",
+                      "sprites/danteFrente2.png", "sprites/danteFrente3.png"]
+listaImagensLadoEsquerdo = ["sprites/danteLadoEsquerdo1.png",
+                            "sprites/danteLadoEsquerdo2.png", "sprites/danteLadoEsquerdo3.png"]
+listaImagensLadoDireito = ["sprites/danteLadoDireito1.png",
+                           "sprites/danteLadoDireito2.png", "sprites/danteLadoDireito3.png"]
+listaImagensCostas = ["sprites/danteCostas1.png",
+                      "sprites/danteCostas2.png", "sprites/danteCostas3.png"]
+
+#======================================================================================
